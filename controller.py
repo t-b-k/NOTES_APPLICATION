980e64d7de33137fe78c03e309b676db2a8b75cc
Tatiana <t-b-k@bk.ru>
2024-02-06 10:31:43 +0300

    Added .csv file notes.csv

--- controller.py
+++ controller.py
@@ -1,4 +1,16 @@
-global HELLO_MESSAGE
+from global_data import *
+import view
+import db_connect
 
-def hello() : 
-    print(HELLO_MESSAGE)
+def run() : 
+    view.out(HELLO_MESSAGE)
+    db_init()
+    onward = False
+    while onward : 
+        view.out(MAIN_MENU)
+
+def db_init() : 
+    global data_base
+    view.out(INIT_MESSAGE, data_base)
+
+    
---
+++ view.py
@@ -0,0 +1,4 @@
+import global_data
+
+def out(what_to_show) : 
+    print(what_to_show)
